3d15fa6b3056fb84ff13bbbeab59c554e8e6328a
Negoescu Raul <suntinzona@gmail.com>
2019-03-07 23:34:51 +0200

    dis

--- app/src/main/java/com/example/slotmachines/MainActivity.java
+++ app/src/main/java/com/example/slotmachines/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //just a comment
+        //raul101
     }
 }
