d110f688368ac2a937a281d847d5f230de62353b
Bogdan Chisalescu <bogdan.chisalescu@gmail.com>
2019-03-17 00:32:03 +0200

    Tested onResume() and tried to save state with onSaveInstanceState

--- app/src/main/java/com/example/slotmachines/MainActivity.java
+++ app/src/main/java/com/example/slotmachines/MainActivity.java
@@ -12,9 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //just a comment
-        //raul101
-        //again
+
     }
 
     static int counter;
