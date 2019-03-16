44637f3669c55b56f010ee14d5a4acbc037d2830
Bogdan Chisalescu <bogdan.chisalescu@gmail.com>
2019-03-17 00:17:11 +0200

    Tested onResume() and tried to save state with onSaveInstanceState

--- app/src/main/java/com/example/slotmachines/MainActivity.java
+++ app/src/main/java/com/example/slotmachines/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.slotmachines;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //just a comment
+    }
+
+    static int counter;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Button pressME = findViewById(R.id.button);
+        final TextView display = findViewById(R.id.hello);
+
+        pressME.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                display.setText(Integer.toString(counter));
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+
+        counter = outState.getInt("counter");
+        super.onSaveInstanceState(outState);
     }
 }
