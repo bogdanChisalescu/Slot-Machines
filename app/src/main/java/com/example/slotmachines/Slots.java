872eae81c88deed6a9f50ad5e03898ccb984d02a
Negoescu Raul <suntinzona@gmail.com>
2019-03-30 17:55:47 +0200

    adaugat ecran pe care afiezi informatii generale si comenatrii

--- app/src/main/java/com/example/slotmachines/Slots.java
+++ app/src/main/java/com/example/slotmachines/Slots.java
@@ -76,13 +76,12 @@ public class Slots extends AppCompatActivity {
         BalanceText.setText(Float.toString(money));
 
         //image1.setBackgroundResource(R.drawable.img1);
-        // image2.setBackgroundResource(R.drawable.img2);
-        for(i=0;i<75;i++) {
+        for(i=0;i<75;i++) {   //loads the image views into image array
             image.add((ImageView) findViewById(getResources().getIdentifier("imageView" + (i + 1), "id", getPackageName())));
         }
 
 
-        for(i=0;i<15;i++) {
+        for(i=0;i<15;i++) {   //defines the animation type and duration for the image array elements
             ecranul.add(ObjectAnimator.ofFloat(image.get(i), "translationY",  0, 2800));
             ecranul.get(i).setDuration(1000);
             cascada.add(ObjectAnimator.ofFloat(image.get(i+15), "translationY", -2800, 0));
@@ -119,12 +118,13 @@ public class Slots extends AppCompatActivity {
                     Raul.calculateWins();
                     win = Raul.returnWin();
 
+                    //loads animations into each animator set
                     AnimCas.play(cascada.get(0)).with(cascada.get(1)).with(cascada.get(2)).with(cascada.get(3)).with(cascada.get(4)).with(cascada.get(5)).with(cascada.get(6)).with(cascada.get(7)).with(cascada.get(8)).with(cascada.get(9)).with(cascada.get(10)).with(cascada.get(11)).with(cascada.get(12)).with(cascada.get(13)).with(cascada.get(14));
                     AnimCas1.play(cascada1.get(0)).with(cascada1.get(1)).with(cascada1.get(2)).with(cascada1.get(3)).with(cascada1.get(4)).with(cascada1.get(5)).with(cascada1.get(6)).with(cascada1.get(7)).with(cascada1.get(8)).with(cascada1.get(9)).with(cascada1.get(10)).with(cascada1.get(11)).with(cascada1.get(12)).with(cascada1.get(13)).with(cascada1.get(14));
                     AnimCas2.play(cascada2.get(0)).with(cascada2.get(1)).with(cascada2.get(2)).with(cascada2.get(3)).with(cascada2.get(4)).with(cascada2.get(5)).with(cascada2.get(6)).with(cascada2.get(7)).with(cascada2.get(8)).with(cascada2.get(9)).with(cascada2.get(10)).with(cascada2.get(11)).with(cascada2.get(12)).with(cascada2.get(13)).with(cascada2.get(14));
                     AnimCas3.play(cascada3.get(0)).with(cascada3.get(1)).with(cascada3.get(2)).with(cascada3.get(3)).with(cascada3.get(4)).with(cascada3.get(5)).with(cascada3.get(6)).with(cascada3.get(7)).with(cascada3.get(8)).with(cascada3.get(9)).with(cascada3.get(10)).with(cascada3.get(11)).with(cascada3.get(12)).with(cascada3.get(13)).with(cascada3.get(14));
                     AnimEcr.play(ecranul.get(0)).with(ecranul.get(1)).with(ecranul.get(2)).with(ecranul.get(3)).with(ecranul.get(4)).with(ecranul.get(5)).with(ecranul.get(6)).with(ecranul.get(7)).with(ecranul.get(8)).with(ecranul.get(9)).with(ecranul.get(10)).with(ecranul.get(11)).with(ecranul.get(12)).with(ecranul.get(13)).with(ecranul.get(14));
-                    AnimCas.start();
+                    AnimCas.start(); //comands the start of the animations
                     AnimCas1.start();
                     AnimCas2.start();
                     AnimCas3.start();
